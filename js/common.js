fca99a3f99546ad0b4284846804edf6a2476b7f4
victorlundman <victor.lundman@elev.ga.ntig.se>
2025-02-28 15:52:06 +0100

    Hero done

---
+++ js/common.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("header");
+
+addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
